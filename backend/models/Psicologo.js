41e1c5eda564ae788e26be1bdd705fea04d8f0ed
Eric Rodrigo Arana Martinez <103905536+Klsie@users.noreply.github.com>
2025-12-20 18:46:39 -0600

    Refactor Psicologo model: update create method and add findById, findAll methods

--- backend/models/Psicologo.js
+++ backend/models/Psicologo.js
@@ -1,13 +1,52 @@
+import { ObjectId } from "mongodb";
 import dbClient from "../config/dbclient.js";
 
 class Psicologo {
     constructor(){
-
+        this.colPsicologos = dbClient.db.collection('psicologos');
     }
-    async create(datosUsuario){
-        colPsicologos = dbClient.db.collection('psicologos');
-        const resultado = await colPsicologos.insertOne(datosUsuario);
-        return resultado;
+
+    async create(datosPsicologo){
+        try {
+            const psicologo = {
+                idPsicologo: new ObjectId(datosPsicologo.idPsicologo),
+                password: datosPsicologo.password,
+                nombre: datosPsicologo.nombre,
+                apellido: datosPsicologo.apellido,
+                fechaInicio: datosPsicologo.fechaInicio,
+                fechaFin: datosPsicologo.fechaFin,
+                cedula: datosPsicologo.cedula,
+                email: datosPsicologo.email,
+                fotoPerfil: datosPsicologo.fotoPerfil,
+            };
+            const resultado = await this.colPsicologos.insertOne(psicologo);
+            return {
+                success: true,
+                message: 'Psicologo creado exitosamente',
+                data: resultado
+            };
+
+        } catch (error) {
+            throw new Error('Error al crear el psicologo: ' + error.message);
+        }
+    }
+
+    async findById(idPsicologo){
+        try {
+            const psicologo = await this.colPsicologos.findOne({ idPsicologo: new ObjectId(idPsicologo) });
+            return psicologo;
+        } catch (error) {
+            throw new Error('Error al buscar el psicologo por ID: ' + error.message);
+        }
+    }
+
+    async findAll(){
+        try {
+            const psicologos = await this.colPsicologos.find({}).toArray();
+            return psicologos;
+        } catch (error) {
+            throw new Error('Error al obtener los psicologos: ' + error.message);
+        }
     }
 }
 
