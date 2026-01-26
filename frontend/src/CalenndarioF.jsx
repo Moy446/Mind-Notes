64db4fbd296b7f6e4c359bccded5291c6e4d31ad
Moy446 <a22310402@ceti.mx>
2026-01-26 17:03:07 -0600

    Hotfix
    Recarga automatica al calendario cuando se actualiza un usuario

--- frontend/src/CalenndarioF.jsx
+++ frontend/src/CalenndarioF.jsx
@@ -35,7 +35,7 @@ export default function CalendarioF(props) {
     }
     useEffect(()=>{
         cargarCitas();
-    },[])
+    },[citas])
 
     const [addMenu, openAddMenu] = useState(false);
     const handleAdd = useCallback((e) => {
