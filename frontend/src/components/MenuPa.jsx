6ae78aea948c05b10ead4081575d3edb68e77c60
Teisel <lesiet02@gmail.com>
2026-01-30 22:29:32 -0600

    hotfix interfaz error

--- frontend/src/components/MenuPa.jsx
+++ frontend/src/components/MenuPa.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './MenuPa.css'
 import { useState } from 'react';
-import { Link } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 export default function MenuPa() {
 
