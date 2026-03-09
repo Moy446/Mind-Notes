2087c6fbce97d13d48249bf872e11b76e137e97d
Teisel <lesiet02@gmail.com>
2026-03-08 22:15:15 -0600

    pruebas

--- backend/controllers/grabacionController.js
+++ backend/controllers/grabacionController.js
@@ -4,26 +4,28 @@ import consumeAI from "../helpers/consumeAI.js";
 import fs from "fs";
 import emailService from "../helpers/emailService.js";
 import path from "path";
+import { Document, Packer, Paragraph, TextRun } from "docx";
+import FileSaver from "file-saver";
 
 class GrabacionController {
     constructor() {
     }
 
     async loadPacientes(req, res) {
-        
+
         const idPsicologo = req.user.idUsuario;
 
         try {
             const listaVinculacion = new ListaVinculacion();
             const nombresPacientes = await listaVinculacion.findByPsicologo(idPsicologo);
-            res.status(200).json({success:true, nombresPacientes});
+            res.status(200).json({ success: true, nombresPacientes });
         } catch (error) {
-            res.status(500).json({ success: false, message: 'Error al cargar la lista de pacientes'});    
+            res.status(500).json({ success: false, message: 'Error al cargar la lista de pacientes' });
         }
     }
 
     async guardarGrabacion(req, res) {
-        const {idPaciente, nombrePaciente, resume, grabacion} = req.body;
+        const { idPaciente, nombrePaciente, resume, grabacion } = req.body;
         const idPsicologo = req.user.idUsuario;
 
         try {
@@ -33,7 +35,7 @@ class GrabacionController {
             //const diarization = await consumeAI.diarize(/*req.file.path*/ filePath);
             const text = await consumeAI.transcribe(/*req.file.path*/ filePath);
             const summaClassi = await consumeAI.classify(text.data.transcription);
-            const {VIDA_LABORAL: vidaLaboral, VIDA_PERSONAL: vidaPersonal, VIDA_AMOROSA: vidaAmorosa, VIDA_FAMILIAR: vidaFamiliar, resumen} = summaClassi.clasificacion;
+            const { VIDA_LABORAL: vidaLaboral, VIDA_PERSONAL: vidaPersonal, VIDA_AMOROSA: vidaAmorosa, VIDA_FAMILIAR: vidaFamiliar, resumen } = summaClassi.clasificacion;
             console.log(vidaLaboral, vidaPersonal, vidaAmorosa, vidaFamiliar, resumen);
 
             //Enviar datos para la elaboracion del archivo
@@ -43,19 +45,33 @@ class GrabacionController {
             console.log(vidaFamiliar);
             console.log(resumen);
             console.log(text);
-                
+
+            const doc = new Document({
+                sections: [{
+                    children: [
+                        new Paragraph({
+                            children: [new TextRun(text)],
+                        }),
+                    ],
+                }],
+            });
+
+            Packer.toBlob(doc).then((blob) => {
+                FileSaver.saveAs(blob, "ejemplo.docx");
+            });
+
             //subir archivo a la base de datos
             const chat = new Chat();
             const resultadoExpediente = await chat.insertExpediente(idPsicologo, idPaciente, docPath);
-            if (resultadoExpediente.modifiedCount !== 1){
+            if (resultadoExpediente.modifiedCount !== 1) {
                 return res.status(500).json({ success: false, message: 'Error al guardar el archivo' });
             }
-            if(grabacion){
+            if (grabacion) {
                 const resultadoGrabacion = await chat.insertGrabacion(idPsicologo, idPaciente, filePath);
                 if (resultadoGrabacion.modifiedCount !== 1) {
                     return res.status(500).json({ success: false, message: 'Error al guardar la grabación' });
                 }
-            }else{
+            } else {
                 const filePath = path.resolve(req.file.path);
                 fs.unlinkSync(filePath);
             }
@@ -68,7 +84,7 @@ class GrabacionController {
             console.log("Error al procesar la grabación:", error);
             return res.status(500).json({ success: false, message: 'Error al procesar la grabación' });
         }
-        
+
     }
 }
 
