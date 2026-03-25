55e225e46226872fcaa4d9048e794cb87e65cfc0
Moy446 <a22310402@ceti.mx>
2026-03-24 22:26:59 -0600

    vista tab del psicologo

--- mobile/Mind-Notes/app/(tabs)/_layout.tsx
+++ mobile/Mind-Notes/app/(tabs)/_layout.tsx
@@ -1,1 +1,95 @@
 //Archivo creado para manejar los tabs que se creen
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { tabStyle } from '../../styles/tabStyle';
+import TabBarButton from '@/components/tabs/TabBarButton';
+import { Image } from 'react-native';
+import { Colors } from '@/constants/theme';
+
+const _layout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        headerStyle: tabStyle.generalTab,
+        headerTitle: '',
+        headerLeft: () => (
+          <Image
+          source={require('../../assets/images/logocolor.png')}
+          style={{ width: 50, height: 50, marginLeft: 15 }}
+          />
+        ),
+        tabBarStyle: {
+          ...tabStyle.generalTab,
+          elevation: 0,
+          shadowColor: 'transparent',
+          borderTopWidth: 0,
+          overflow: 'hidden',
+        },
+        tabBarActiveTintColor: Colors.white,
+        
+
+      }}
+      >
+        <Tabs.Screen 
+          name='grabacion/index' 
+          options={{
+            tabBarButton: (props) => (
+              <TabBarButton 
+                {...props} 
+                icon='record'
+                route="grabacion"
+                
+              >
+              Grabacion
+              </TabBarButton>
+            )
+          }}
+        />
+        <Tabs.Screen 
+          name='calendario/index' 
+          options={{
+            tabBarButton: (props) => (
+              <TabBarButton 
+                {...props} 
+                icon='calendar'
+                route="calendario"
+              >
+              Calendario
+              </TabBarButton>
+            )
+          }}
+        />
+        <Tabs.Screen 
+          name='chat/index' 
+          options={{
+            tabBarButton: (props) => (
+              <TabBarButton 
+                {...props} 
+                icon='chat'
+                route="chat"
+              >
+              Chat
+              </TabBarButton>
+            )
+          }}
+
+        />
+        <Tabs.Screen 
+          name='perfil/index' 
+          options={{
+            tabBarButton: (props) => (
+              <TabBarButton 
+                {...props} 
+                icon='profile'
+                route="perfil"
+              >
+              Perfil
+              </TabBarButton>
+            )
+          }}
+        />
+    </Tabs>
+  )
+}
+
+export default _layout
---
+++ mobile/Mind-Notes/app/(tabs)/calendario/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Colors } from '@/constants/theme'
+
+const CalendarioScreen = () => {
+  return (
+    <View style={{backgroundColor: Colors.background}}>
+      <Text>CalendarioScreen</Text>
+    </View>
+  )
+}
+
+export default CalendarioScreen
