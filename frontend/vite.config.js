9d58db66aefe2463e30614c63e057287794d9d66
Moy446 <a22310402@ceti.mx>
2026-04-13 11:18:33 -0600

    fix vite error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_BACKEND_BASE_URL,
+        target: process.env.VITE_BACKEND_BASE_URL,
         changeOrigin: true,
       },
     },
